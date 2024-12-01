342c7fa7cb647a4e0546954954af5af38e4de18e
MomusWinner <areg06.06@mail.ru>
2024-12-01 23:29:42 +0300

    feat: add const

---
+++ src/const.js
@@ -0,0 +1,9 @@
+export const APP_ROUTE = {
+    main : "/main",
+    services : "/services/",
+    auth: "/login",
+    signin: "/singin"
+}
+
+export const API_PATH = "https://673423afa042ab85d1190055.mockapi.io/api/v1"
+export const API_AUTH_PATH = "https://dummyjson.com/auth"
