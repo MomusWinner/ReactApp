5972172f1ca7777e5f7a039a8c36f937034c7c8e
MomusWinner <areg06.06@mail.ru>
2024-12-01 23:31:12 +0300

    feat: add api action

---
+++ src/store/api-actions.js
@@ -0,0 +1,27 @@
+import { API_AUTH_PATH, API_PATH } from "../const";
+import axios from "axios";
+
+export const getNewsListAction = async function(){
+    const apiUrl = API_PATH + '/main';
+    let response = await axios.get(apiUrl);
+    return await response.data
+}
+
+export const getServiceListAction = async function () {
+    const apiUrl = API_PATH + '/services';
+    let response = await axios.get(apiUrl);
+    return response.data
+}
+
+export const getServiceAction = async function(id) {
+    const url = API_PATH + '/services/' + id
+    let response = await axios.get(url)
+    return response.data
+}
+
+export const signin = async function(user) {
+    const url = API_AUTH_PATH + '/login'
+    console.log(user)
+    let response = await axios.post(url, user, {headers: {'Content-Type': 'application/json'}})
+    return response
+}
