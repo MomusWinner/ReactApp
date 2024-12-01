62814fc70af40a942c58cafdfe522a802f5dafe3
MomusWinner <areg06.06@mail.ru>
2024-12-02 01:14:12 +0300

    fix: add profile to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import {Button} from "@consta/uikit/Button"
-import {User} from "@consta/uikit/User"
 import { useNavigate } from "react-router-dom"
 import { APP_ROUTE } from "../../const"
 import { useSelector } from "react-redux"
@@ -10,7 +9,6 @@ import { useDispatch } from "react-redux"
 const Header = function() {
     const user = useSelector(state => state.user.user)
     const dispatch = useDispatch()
-
     const navigate = useNavigate()
 
     var authButton = null
@@ -25,12 +23,10 @@ const Header = function() {
         authButton = <Button label="Выход" onClick={exit} /> 
     }
 
-
-
     return (
         <header style={{
             display: "flex",
-            justifyContent: "space-evenly",
+            justifyContent: "space-around",
             position: "sticky",
             top: 0,
             paddingBottom: "20px",
@@ -38,7 +34,9 @@ const Header = function() {
         <div>
             <Button label="Главная страница" onClick={() => navigate(APP_ROUTE.main)} />
             <Button label="Услуги компании" onClick={() => navigate(APP_ROUTE.services)} />
-            <Button label={ user !== null ? user?.firstName + " " + user?.lastName: "Гость"}></Button>
+            <Button
+                label={ user !== null ? user?.firstName + " " + user?.lastName: "Гость"}
+                onClick={()=>navigate(APP_ROUTE.profile)}/>
             {authButton}
         </div>
         </header>
