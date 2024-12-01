762282009147dcd8b94d6e394066c4b0610877fd
MomusWinner <areg06.06@mail.ru>
2024-12-02 00:01:56 +0300

    fix: drop auth on exit

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,20 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
-import {saveToken, getToken} from "../services/token"
+import {saveToken, dropToken, getToken} from "../services/token"
 
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        user: localStorage.getItem("user"),
+        user: JSON.parse(localStorage.getItem("user")),
         token: getToken()
     },
     reducers: {
         setUser: (state, action) => {
             saveToken(action.payload.token)
-            localStorage.setItem("user", action.payload.user)
+            localStorage.setItem("user", JSON.stringify(action.payload.user))
             state.user = action.payload.user;
             state.token = action.payload.token;
         },
         clearUser: (state) => {
+            dropToken()
+            localStorage.removeItem("user")
+            console.log("drop user")
             state.user = null;
             state.token = '';
         }
