68e4a699ed1b8de9a6e55bde7b5fca7b143d944a
MomusWinner <areg06.06@mail.ru>
2024-12-02 01:13:58 +0300

    feat: Profile page

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux"
+import { APP_ROUTE } from "../../const"
+import { useNavigate } from "react-router"
+import { Avatar } from '@consta/uikit/Avatar';
+
+const Profile = function(){
+    const navigate = useNavigate()
+    const user = useSelector(state => state.user.user)
+
+    if (user === null)
+        return navigate(APP_ROUTE.signin)
+    return(
+    <>
+        <div style={{
+            display: "flex",
+            justifyContent: "space-around"
+        }}>
+            <div>
+                <h1>Логин: {user.username}</h1>
+                <h3>Имя: {user.firstName}</h3>
+                <h3>Фамилия: {user.lastName}</h3>
+                <h3>Почта: {user.email}</h3>
+            </div>
+            <Avatar
+                style={{
+                    width: "250px",
+                    height: "250px"
+                }}
+                url={user.image}>
+            </Avatar>
+        </div>
+    </>)
+}
+
+export default Profile
