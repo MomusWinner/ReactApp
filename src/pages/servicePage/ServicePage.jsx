8f6aa7969b53e5b69007f05a72cda1d1705bd1e7
MomusWinner <areg06.06@mail.ru>
2024-12-03 11:47:56 +0300

    fix: servicePage error on get invalid id

--- src/pages/servicePage/ServicePage.jsx
+++ src/pages/servicePage/ServicePage.jsx
@@ -3,16 +3,20 @@ import { useParams } from "react-router";
 import { getServiceAction } from "../../store/api-actions";
 import { Loader } from "@consta/uikit/Loader";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { APP_ROUTE } from "../../const";
 
 const ServicePage = () => {
     let { serviceId } = useParams();
-    const [service, setService] = useState(useSelector(state => state.services.value).find(s => s.id == serviceId));
+    const [service, setService] = useState(useSelector(state => state.services.value)?.find(s => s.id == serviceId));
+    const navigate = useNavigate()
 
     useEffect(() => {
-            if(service)
+        if(service == null)
             getServiceAction(serviceId).then(service=>{
+                console.log("request")
                 setService(service)
-            })
+            }).catch(_=>{navigate(APP_ROUTE.services)})
     }, []);
 
     if(service === null){
@@ -21,9 +25,9 @@ const ServicePage = () => {
 
     return (
         <>
-            <h1>{service.name}</h1>
-            <img src={service.image}/>
-            <p>{service.description}</p>
+            <h1>{service?.name}</h1>
+            <img src={service?.image}/>
+            <p>{service?.description}</p>
         </>
     )
 }
