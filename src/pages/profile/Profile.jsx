1ac9d201a62839b1158891132ad8a24a8d69192e
MomusWinner <areg06.06@mail.ru>
2024-12-03 11:38:34 +0300

    fix: move auth check logic to CheckAuth component

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,15 +1,8 @@
 import { useSelector } from "react-redux"
-import { APP_ROUTE } from "../../const"
-import { useNavigate } from "react-router"
 import { Avatar } from '@consta/uikit/Avatar';
-import { useEffect } from "react";
 
 const Profile = function(){
-    const navigate = useNavigate()
     const user = useSelector(state => state.user.user)
-    useEffect(()=>{
-        return navigate(APP_ROUTE.signin)
-    }, [])
 
     return(
     <>
@@ -18,17 +11,17 @@ const Profile = function(){
             justifyContent: "space-around"
         }}>
             <div>
-                <h1>Логин: {user.username}</h1>
-                <h3>Имя: {user.firstName}</h3>
-                <h3>Фамилия: {user.lastName}</h3>
-                <h3>Почта: {user.email}</h3>
+                <h1>Логин: {user?.username}</h1>
+                <h3>Имя: {user?.firstName}</h3>
+                <h3>Фамилия: {user?.lastName}</h3>
+                <h3>Почта: {user?.email}</h3>
             </div>
             <Avatar
                 style={{
                     width: "250px",
                     height: "250px"
                 }}
-                url={user.image}>
+                url={user?.image}>
             </Avatar>
         </div>
     </>)
